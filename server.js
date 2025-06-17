02fdbc4b4b778422001b4a958f1e46ac6534bd9f
KuraidoXenpaI <asdwera996@gmail.com>
2025-06-18 04:33:56 +0800

    Layout Fixes: * Fixed the login page * Fixed the Sign-up page * Fixed the Forgot Password page * Fixed the Landing page * Fixed the pcBuilder page * Fixed the Shop page
    Updates:
    * Margins, Paddings, Font-sizes
    * Organized the file structures
    * navbar and footer layout update
    * linked pages to landing screen
    
    Todos:
    * re-structured the files in a its easy to work
    * turn necessary code to partials

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import homePage from"./routes/pages/homePage.js";
+import homePage from "./routes/pages/homePage.js";
 import auth from "./routes/pages/auth.js";
 import shopPage from "./routes/pages/shopPage.js";
 import pcBuilder from "./routes/pages/pcBuilder.js";
@@ -14,33 +14,33 @@ const PORT = 8000;
 // Use body-parser middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- 
+
 // use the static middleware to serve static files
 app.use(express.static("public"));
 
 //initializing EJS and Statics
-app.set('view engine','ejs');
-app.use(express.static('styles'));
-app.use(express.static('assets'));
-app.use(express.static('scripts'));
-app.use(express.static('views'));
+app.set("view engine", "ejs");
+// app.use(express.static("styles"));
+app.use(express.static("assets"));
+app.use(express.static("scripts"));
+app.use(express.static("views"));
+
 
 //using routers
 app.use("/", homePage);
-app.use("/auth",auth);
-app.use("/shop",shopPage)
-app.use("/pcbuilder",pcBuilder);
-app.use("/pcprofile",pcProfile);
-app.use("/profile",userProfile);
+app.use("/auth", auth);
+app.use("/shop", shopPage);
+app.use("/pcbuilder", pcBuilder);
+app.use("/pcprofile", pcProfile);
+app.use("/userProfile", userProfile);
 app.use("/api", usersRouter);
 
 connect();
 
-app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
 });
 
-app.use((req,res,next)=>{
-    res.send('404 not found');
+app.use((req, res, next) => {
+  res.send("404 not found");
 });
-
