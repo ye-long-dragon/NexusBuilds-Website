c78c22b80e44761f958a72de869e5cd982a6de84
ye-long-dragon <bvincelawrence@gmail.com>
2025-07-02 00:14:47 +0800

    Finished all buttons, proceed witk api routing

--- routes/pages/shopAdmin.js
+++ routes/pages/shopAdmin.js
@@ -2,11 +2,11 @@ import express from "express";
 const shopAdmin = express.Router();
 
 shopAdmin.get('/',(req,res)=>{
-    res.render('myProducts/index');
+    res.render('myProducts/index',{products: null});
 });
 
 shopAdmin.get('/shiporders',(req,res)=>{
-    res.render('shipOrders/index');
+    res.render('shipOrders/index',{shipOrders: null});
 });
 
 shopAdmin.get('/addproduct',(req,res)=>{
